02c29b62a89cba54a3bf833874763a918f4796df
Isabely Perez Marques <isabely.pmarques.mult@gmail.com>
2026-05-28 18:18:08 -0300

    teste de renderização 2

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div style={{ padding: '50px', textBreak: 'break-word', textAlign: 'center' }}>
+    <div style={{ padding: '50px', wordBreak: 'break-word', textAlign: 'center' }}>
       <h1 style={{ color: '#000', fontSize: '32px' }}>O React está funcionando!</h1>
       <p style={{ color: '#666' }}>Se você está lendo isso, o index.html e o Vite estão configurados certos.</p>
     </div>
