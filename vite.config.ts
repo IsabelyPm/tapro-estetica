6040319b7e6d74e0bce105fa612a848cfd3df93d
Isabely Perez Marques <isabely.pmarques.mult@gmail.com>
2026-05-28 17:34:41 -0300

    Fixing bugs tanstack vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from '@tanstack/start/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
+// Se o @tanstack/start/config der erro ao instanciar, o Vinxi lê nativamente assim:
 export default defineConfig({
-  vite: {
-    plugins: [
-      tsconfigPaths(),
-    ],
-  },
-})
+  plugins: [
+    tsconfigPaths(),
+  ],
+});
